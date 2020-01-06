529c20fbc56d77fc4bce660e3bacc74d53be05e6
kylianvc <kylianvancanneyt@gmail.com>
2020-01-06 21:02:22 +0100

    Split pagina's

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import React, { Component } from "react";
+import Detail from "./pages/Detail";
+import Overzicht from "./pages/Overzicht";
+import Page404 from "./pages/Page404";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <Router>
+        
+        <Switch>
+        <Route path="/Detail" component={Detail} />
+        <Route path="/" exact component={Overzicht} />
+        <Route path="/" component={Page404} />
+      </Switch>
+        <div>
         <h1>Hello to u 2</h1>
-      </div>
+        </div>
+      </Router>
+      
     );
   }
 }
---
+++ src/pages/Overzicht.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Overzicht extends Component {
+    render() {
+        return (
+            <div>
+                overzicht
+            </div>
+        );
+    }
+}
+
+export default Overzicht;

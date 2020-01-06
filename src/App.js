58ff5c5b3bc043bbe4941ed0b7739a7f5e3d22f6
Carl_Vanhaelemeersch <carl.vanhaelemeersch@gmail.com>
2020-01-06 21:09:15 +0100

    Navbar added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React, { Component } from "react";
+import bootstrap from "bootstrap/dist/css/bootstrap.min.css";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <h1>Hello to u 2</h1>
+        <nav class="navbar navbar-light bg-dark justify-content-center bgGradient mb-4">
+          <a class="navbar-brand" href="http://localhost:3000/">
+            <img
+              src="../src/images/tmdb_nobg.png"
+              width="125"
+              height="125"
+              alt="logo"
+            />
+          </a>
+        </nav>
       </div>
     );
   }

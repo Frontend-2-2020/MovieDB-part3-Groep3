81e757211970283a1225b0ae9ecd8ebfb0e8411c
kylianvc <kylianvancanneyt@gmail.com>
2020-01-06 21:30:15 +0100

    Api-key

--- src/pages/Overzicht.js
+++ src/pages/Overzicht.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
+import Axios from 'axios';
 
 class Overzicht extends Component {
+
+componentDidMount(){
+    this.getMovies();
+}
+
+getMovies(){
+Axios.get("https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=4bc044ddb757b980e41e9e71ef3aafae")
+.then(response =>{
+    console.log(response);
+})
+};
     render() {
         return (
             <div>
-                overzicht
+                
             </div>
         );
     }

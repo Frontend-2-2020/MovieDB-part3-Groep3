48e7805243be882d43dafdfd8af79937c528993e
kylianvc <kylianvancanneyt@gmail.com>
2020-01-06 21:41:01 +0100

    Component files set-up

---
+++ src/Components/ProductionCompanyList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ProductionCompanyList extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default ProductionCompanyList;

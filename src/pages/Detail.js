575c451cbe0ad60ca5e28608d313bc0df394d72f
Carl_Vanhaelemeersch <carl.vanhaelemeersch@gmail.com>
2020-01-19 21:14:08 +0100

    Update Pagination

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -1,62 +1,63 @@
 import React, { Component } from "react";
 import { KEY } from "../config";
 import Axios from "axios";
-import "../style/Detail.css";
+// import "../style/Detail.css";
 import ProductionCompanyList from "../Components/ProductionCompanyList";
 
 class Detail extends Component {
-  
-    state = {
-      movieInfo: [{genres:[], production_companies:[]}]
-    };
-  
+  state = {
+    movieInfo: [{ genres: [], production_companies: [] }]
+  };
 
   // Excuting our Axios Call once to avoid an never-ending loop
   componentDidMount() {
     const { MovieDetail } = this.props.match.params;
-   
+
     this.getDetail(MovieDetail);
-    
   }
-  
+
   // Making our Axios Call en updating our state
   getDetail(id) {
-    
     Axios.get(
-      "https://api.themoviedb.org/3/movie/"+id+"?api_key=" +
-        KEY
+      "https://api.themoviedb.org/3/movie/" + id + "?api_key=" + KEY
     ).then(response => {
-     
       this.setState({ movieInfo: response.data });
       console.log(this.state);
     });
   }
-  
+
   render() {
     const data = this.state.movieInfo;
 
-    return <div className="jumbotron jumbotron-fluid mt-0 h-200 pt-2 pb-2" style={{
-      backgroundImage: `url('https://image.tmdb.org/t/p/original${data.backdrop_path} ')`
-    }}>
+    return (
+      <div
+        className="jumbotron jumbotron-fluid mt-0 h-200 pt-2 pb-2"
+        style={{
+          backgroundImage: `url('https://image.tmdb.org/t/p/original${data.backdrop_path} ')`
+        }}
+      >
         <div className="container-fluid">
-            <img className="float-left mr-4 img-fluid pt-5" src={"https://image.tmdb.org/t/p/w300/"+ data.poster_path}  alt=""/>
-         
-            <div className="" >
-            <h1 className="display-4 pt-5">{data.title} <span className="small font-weight-light text-secondary">({data.release_date})</span></h1>
+          <img
+            className="float-left mr-4 img-fluid pt-5"
+            src={"https://image.tmdb.org/t/p/w300/" + data.poster_path}
+            alt=""
+          />
+
+          <div className="">
+            <h1 className="display-4 pt-5">
+              {data.title}{" "}
+              <span className="small font-weight-light text-secondary">
+                ({data.release_date})
+              </span>
+            </h1>
             <p className="lead">score: {data.vote_average}</p>
-            
+
             <p>{data.overview}</p>
-            <div>
-            
-              
-            </div>
-            
-        </div> 
+            <div></div>
+          </div>
         </div>
-        
-        
-        
-      </div>;
+      </div>
+    );
   }
 }
 
--- src/pages/Overzicht.js
+++ src/pages/Overzicht.js
@@ -1,42 +1,56 @@
 import React, { Component } from "react";
 import { KEY } from "../config.js";
+import Pagination from "rc-pagination";
+import "rc-pagination/assets/index.css";
 import Axios from "axios";
 
 import MovieCard from "../Components/MovieCard";
 import ScrollButton from "../Components/ScrollButton";
-import Pagination from "../Components/Pagination.js";
 
 class Overzicht extends Component {
   // Defining our state with an empty array
   state = {
     movies: [],
-    totalPages: []
+    dataList: [],
+    page: 1
   };
 
   // Excuting our Axios Call once to avoid an never-ending loop
   componentDidMount() {
     this.getMovies();
   }
-
   // Making our Axios Call en updating our state
   getMovies() {
     Axios.get(
-      "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=" +
+      "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&page=" +
+        this.state.page +
+        "&api_key=" +
         KEY
       //https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&page=" + page + "&api_key=" + KEY
     ).then(response => {
       this.setState({
         movies: response.data.results,
-        totalPages: response.data
+        dataList: response.data
       });
+      console.log(response.data);
     });
   }
+  onChange = current => {
+    console.log(current);
+    this.setState({
+      page: current
+    });
+  };
 
   // Making our container filled with our MovieCards and passing our props
   render() {
     return (
       <div className="container-fluid">
-        <Pagination />
+        <Pagination
+          onChange={this.onChange}
+          page={this.state.page}
+          total={this.state.dataList.total_pages}
+        />
         <div className="row">
           {this.state.movies.map((movie, index) => (
             <MovieCard movie={movie} />

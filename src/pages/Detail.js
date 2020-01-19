af6450258421665663cc3c51bc449cae8b4d30ab
kylianvc <kylianvancanneyt@gmail.com>
2020-01-19 17:59:01 +0100

    images

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -2,11 +2,12 @@ import React, { Component } from "react";
 import { KEY } from "../config";
 import Axios from "axios";
 import "../style/Detail.css";
+import ProductionCompanyList from "../Components/ProductionCompanyList";
 
 class Detail extends Component {
   
     state = {
-      movieInfo: []
+      movieInfo: [{genres:[], production_companies:[]}]
     };
   
 
@@ -33,18 +34,24 @@ class Detail extends Component {
   
   render() {
     const data = this.state.movieInfo;
-    const jumbotImg = "https://image.tmdb.org/t/p/w500/" +data.backdrop_path;
-    console.log(jumbotImg);
-    return <div className="jumbotron jumbotron-fluid mt-0" >
-        <div className="container container-fluid">
-            <img className="float-left mr-4 img-fluid" src={"https://image.tmdb.org/t/p/w500/"+ data.poster_path}  alt=""/>
-            {/* <img src={jumbotImg} class="background" alt=""/> */}
+
+    return <div className="jumbotron jumbotron-fluid mt-0 h-200 pt-2 pb-2" style={{
+      backgroundImage: `url('https://image.tmdb.org/t/p/original${data.backdrop_path} ')`
+    }}>
+        <div className="container-fluid">
+            <img className="float-left mr-4 img-fluid pt-5" src={"https://image.tmdb.org/t/p/w300/"+ data.poster_path}  alt=""/>
+         
             <div className="" >
-            <h1 className="display-4">{data.title} ({data.release_date})</h1>
+            <h1 className="display-4 pt-5">{data.title} <span className="small font-weight-light text-secondary">({data.release_date})</span></h1>
             <p className="lead">score: {data.vote_average}</p>
             
             <p>{data.overview}</p>
-        </div>
+            <div>
+            
+              
+            </div>
+            
+        </div> 
         </div>
         
         
